c322b83ed238cfa4e6fe37b566d247c6c07912c2
Ed Sweeney <ed@onextent.com>
2018-04-11 19:30:09 -0700

    init

---
+++ src/main/scala/onextent/test/dd/models/Message.scala
@@ -0,0 +1,10 @@
+package onextent.test.dd.models
+
+import java.util.{Date, UUID}
+
+final case class Message(
+    id: UUID,
+    datetime: Date,
+    body: String
+)
+
---
+++ src/main/scala/onextent/test/dd/routes/ApiRoute.scala
@@ -0,0 +1,31 @@
+package onextent.test.dd.routes
+
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.server.{Directives, Route}
+import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
+import com.typesafe.scalalogging.LazyLogging
+import onextent.test.dd.ErrorSupport
+import onextent.test.dd.models.JsonSupport
+
+object ApiRoute
+    extends JsonSupport
+    with LazyLogging
+    with Directives
+    with ErrorSupport {
+
+  def apply: Route =
+    path(urlpath) {
+      logRequest(urlpath) {
+        handleErrors {
+          cors(corsSettings) {
+            get {
+              logger.debug(s"get $urlpath")
+              complete(
+                HttpEntity(ContentTypes.`application/json`,
+                           "{\"msg\": \"Say hello to Akka Docker Dummy\"}\n"))
+            }
+          }
+        }
+      }
+    }
+}
